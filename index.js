4a88b0205c116177cd2ba6697b6c969604ab5f3e
hansori01 <19sorihan88@gmail.com>
2021-02-06 02:20:36 +0000

    recursion adding nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let output = 0;
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      output += sumItems(item);
+    } else {
+      output += item;
+    }
+  });
+  return output;
 }
+console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+console.log(sumItems([1, 2, 3, 4, 5]));
 
 module.exports = sumItems;
+
